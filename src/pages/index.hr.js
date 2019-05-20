4777137832d58b9381c5c50ab6bb8a6a6c9c05a4
Bartol Deak <bartoldeaaak@gmail.com>
2019-05-20 16:36:12 +0200

    hr index page that shows only hr posts

--- src/pages/index.hr.js
+++ src/pages/index.hr.js
@@ -1,3 +1,24 @@
 import React from 'react'
+import Layout from '../components/layout'
+import usePosts from '../utils/use-posts'
+import PostPreview from '../components/post-preview'
+import { Link } from 'gatsby'
 
-export default () => <div>Pozdrav iz hrvatske!</div>
+export default () => {
+  const posts = usePosts()
+  return (
+    <>
+      <Layout>
+        <h2>Citaj moj blog</h2>
+        <Link to="/hr/">HRV</Link>
+        <Link to="/">ENG</Link>
+        {posts.map(post => {
+          if (post.langKey === 'hr') {
+            return <PostPreview key={post.slug} post={post} />
+          }
+          return null
+        })}
+      </Layout>
+    </>
+  )
+}
