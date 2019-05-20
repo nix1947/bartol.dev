1ebfde14974a4abf63c98bb8bc3ea5956d00c290
Bartol Deak <bartoldeaaak@gmail.com>
2019-05-20 16:35:53 +0200

    en index page that shows only en posts

--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -1,3 +1,24 @@
 import React from 'react'
+import Layout from '../components/layout'
+import usePosts from '../utils/use-posts'
+import PostPreview from '../components/post-preview'
+import { Link } from 'gatsby'
 
-export default () => <div>Hello World!</div>
+export default () => {
+  const posts = usePosts()
+  return (
+    <>
+      <Layout>
+        <h2>Read my blog</h2>
+        <Link to="/hr/">HRV</Link>
+        <Link to="/">ENG</Link>
+        {posts.map(post => {
+          if (post.langKey === 'en') {
+            return <PostPreview key={post.slug} post={post} />
+          }
+          return null
+        })}
+      </Layout>
+    </>
+  )
+}
