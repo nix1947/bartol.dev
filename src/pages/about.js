8e79abf4130a495a935b3f8ad46581e3117befc6
Bartol Deak <bartoldeaaak@gmail.com>
2019-06-07 22:59:44 +0200

    added basic index and about page for testing

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+export default () => (
+  <Layout>
+    <h1>About me</h1>
+    <p>Hi my name is Bartol Deak</p>
+    <Link to="/">Go back to home</Link>
+  </Layout>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+export default () => (
+  <Layout>
+    <h1>Hello...</h1>
+    <p>...world!</p>
+    <Link to="/about/">Learn more about me</Link>
+  </Layout>
+)
