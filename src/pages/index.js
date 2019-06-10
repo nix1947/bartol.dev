77fdb08953cf08e13a0317af4f52aa17db5d4c4f
bartold3ak <b@bartol.dev>
2019-06-10 22:39:18 +0200

    index now shows list of all posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import usePosts from '../utils/use-posts'
+import PostPreview from '../components/post-preview'
 
-export default () => (
-  <Layout>
-    <h1>Hello...</h1>
-    <p>...world!</p>
-    <Link to="/about/">Learn more about me</Link>
-  </Layout>
-)
+export default () => {
+  const posts = usePosts()
+
+  return (
+    <Layout>
+      <h1>Hello...</h1>
+      <p>...world!</p>
+      <Link to="/about/">Learn more about me</Link>
+      <h2>Read my blog. Just do it.</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  )
+}
