7ff15d991e5c06dcafb422407f4db866f3f1b2d6
bartold3ak <b@bartol.dev>
2019-06-10 22:40:11 +0200

    captions for images and code

---
+++ src/components/caption.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+const Caption = styled.figcaption`
+  font-size: 18px;
+  color: gray;
+  margin: 10px 0 0 0;
+  text-align: center;
+`
+
+export default Caption
