75613581de84bbe10314f0a0cd9cba85fc8278a3
bartold3ak <b@bartol.dev>
2019-06-10 22:44:02 +0200

    mdx, remark, analyzer setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,53 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion']
+  siteMetadata: {
+    title: "Bartol's Blog",
+    description: 'site description. add later '
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js')
+        },
+        gatsbyRemarkPlugins: [
+          { resolve: 'gatsby-remark-images' },
+          { resolve: 'gatsby-remark-prismjs' }
+        ]
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static'
+      }
+    }
+    // {
+    //   resolve: 'gatsby-transformer-remark',
+    //   options: {
+    //     plugins: [
+    //       {
+    //         resolve: `gatsby-remark-prismjs`,
+    //         options: {
+    //           showLineNumbers: true
+    //         }
+    //       }
+    //     ]
+    //   }
+    // }
+  ]
 }
