9b290d7baf78aa472c25b0f2de9757b0aa053898
bartold3ak <b@bartol.dev>
2019-06-13 23:49:57 +0200

    sorting posts by date

--- src/utils/use-posts.js
+++ src/utils/use-posts.js
@@ -3,23 +3,29 @@ import { graphql, useStaticQuery } from 'gatsby'
 const usePosts = () => {
   const data = useStaticQuery(graphql`
     query {
-      allMdx {
+      allMdx(
+        sort: { fields: [frontmatter___date], order: DESC }
+        filter: {
+          frontmatter: { published: { eq: true }, langKey: { eq: "en" } }
+        }
+      ) {
         nodes {
           frontmatter {
             title
             slug
             date(formatString: "MMMM Do, YYYY")
             tags
+            published
             image {
               sharp: childImageSharp {
-                fluid(duotone: { shadow: "#212121", highlight: "#299999" }) {
+                fluid(duotone: { shadow: "#222222", highlight: "#089a82" }) {
                   ...GatsbyImageSharpFluid_withWebp
                 }
               }
             }
           }
           timeToRead
-          excerpt(pruneLength: 110)
+          excerpt(pruneLength: 90)
         }
       }
     }
