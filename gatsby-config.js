a5c29527e07557bc37537c960d59643706cc7a5f
bartold3ak <b@bartol.dev>
2019-06-19 23:01:28 +0200

    algolia backend updated, fonts prefetch, pwa and rss setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
+  path: `.env.${process.env.NODE_ENV}`,
 })
 
 const postQuery = `{
@@ -10,9 +10,25 @@ const postQuery = `{
       node {
           frontmatter {
             title
+            slug
             date(formatString: "MMMM Do, YYYY")
+            datetimestamp
             tags
+              image {
+              sharp: childImageSharp {
+                fluid {
+                  src
+                  srcSetWebp
+                  aspectRatio
+                  sizes
+                  base64
+                }
+              }
+              extension
+              publicURL
+            }
           }
+          timeToRead
           excerpt(pruneLength: 100000)
       }
     }
@@ -23,14 +39,15 @@ const queries = [
   {
     query: postQuery,
     transformer: ({ data }) => data.posts.edges.map(({ node }) => node), // optional
-    settings: {}
-  }
+    settings: {},
+  },
 ]
 
 module.exports = {
   siteMetadata: {
     title: "Bartol's Blog",
-    description: 'site description. add later '
+    description: 'site description. add later ',
+    siteUrl: 'https://bartol.dev/',
   },
   plugins: [
     'gatsby-plugin-emotion',
@@ -38,50 +55,150 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-dark-mode',
+    'gatsby-plugin-force-trailing-slashes',
     {
       resolve: 'gatsby-mdx',
       options: {
         defaultLayouts: {
-          default: require.resolve('./src/components/layout.js')
+          default: require.resolve('./src/components/layout.js'),
         },
         gatsbyRemarkPlugins: [
           { resolve: 'gatsby-remark-images' },
           { resolve: 'gatsby-remark-prismjs' },
-          { resolve: 'gatsby-remark-reading-time' }
-        ]
-      }
+          { resolve: 'gatsby-remark-reading-time' },
+          { resolve: 'gatsby-remark-smartypants' },
+          { resolve: 'gatsby-remark-external-links' },
+        ],
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'posts',
-        path: 'posts'
-      }
+        path: './posts/',
+      },
     },
     {
-      resolve: `gatsby-plugin-algolia`,
+      resolve: 'gatsby-plugin-algolia',
       options: {
         appId: process.env.ALGOLIA_APP_ID,
         apiKey: process.env.ALGOLIA_API_KEY,
         indexName: process.env.ALGOLIA_INDEX_NAME, // for all queries
         queries,
-        chunkSize: 10000 // default: 1000
-      }
+        chunkSize: 10000, // default: 1000
+      },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-prefetch-google-fonts',
       options: {
-        trackingId: process.env.GA_TRACKING
-      }
+        fonts: [
+          {
+            family: 'IBM Plex Sans',
+            variants: ['400', '600'],
+          },
+          {
+            family: 'IBM Plex Mono',
+            variants: ['400'],
+          },
+        ],
+      },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: process.env.GA_TRACKING,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: "Bartol's Blog",
+        short_name: "Bartol's Blog",
+        start_url: '/',
+        background_color: '#121212',
+        theme_color: '#f35627',
+        display: 'standalone',
+        icon: 'static/logo.svg',
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-netlify',
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://bartol.dev/',
+        sitemap: 'https://bartol.dev/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            // eslint-disable-next-line max-len
+            serialize: ({ query: { site, allMdx } }) => allMdx.edges.map(edge => Object.assign({}, edge.node.frontmatter, {
+              ...edge.node.frontmatter,
+              description: edge.node.excerpt,
+              url: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
+              guid: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
+            })),
+            query: `
+              {
+                allMdx(
+                  limit: 1000,
+                  sort: {
+                    order: DESC,
+                    fields: [frontmatter___date]
+                  }
+                  filter: { frontmatter: { published: { eq: true } } }
+                ) {
+                  edges {
+                    node {
+                      frontmatter {
+                        title
+                        date
+                        slug
+                      }
+                      excerpt(pruneLength: 100000)
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
+            title: "Bartol's Blog RSS Feed",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: '^/blog/',
+          },
+        ],
+      },
+    },
+    // 'gatsby-plugin-eslint',
     {
       resolve: 'gatsby-plugin-webpack-bundle-analyzer',
       options: {
         production: true,
         disable: !process.env.ANALYZE_BUNDLE_SIZE,
         generateStatsFile: true,
-        analyzerMode: 'static'
-      }
-    }
-  ]
+        analyzerMode: 'static',
+      },
+    },
+  ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -9,21 +9,21 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
         }
       }
     }
-  `)
+  `);
 
   if (result.errors) {
-    reporter.panic('failed to create posts', result.errors)
+    reporter.panic('failed to create posts', result.errors);
   }
 
-  const posts = result.data.allMdx.nodes
+  const posts = result.data.allMdx.nodes;
 
-  posts.forEach(post => {
+  posts.forEach((post) => {
     actions.createPage({
       path: post.frontmatter.slug,
       component: require.resolve('./src/templates/post.js'),
       context: {
-        slug: post.frontmatter.slug
-      }
-    })
-  })
-}
+        slug: post.frontmatter.slug,
+      },
+    });
+  });
+};
