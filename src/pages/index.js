69d0a4e683d0325d962843c66d883e661f5c51fa
bartold3ak <b@bartol.dev>
2019-06-21 01:27:45 +0200

    removed list of posts from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,8 @@
 import React from 'react'
 import Layout from '../components/layout'
-import usePosts from '../utils/use-posts'
-import PostPreview from '../components/post-preview'
-// import Search from '../components/Search'
 
-export default () => {
-  const posts = usePosts()
-
-  return (
-    <Layout>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
-  )
-}
+export default () => (
+  <Layout>
+    <h1>Index.</h1>
+  </Layout>
+)
